d782952f6434e929f3c30cce0ef99cf254c7bc0f
Arthur Pessoa <arthhpb@gmail.com>
2025-05-11 15:28:22 -0300

    Código da Aula 02 adicionado

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,12 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
+        // int, long, short, byte: inteiros
+        // float, double: ponto flutuante
+        // char: caracteres unicode
+        // String: é uma classe do Java
 
+        int idade = 30;
+        System.out.println("A idade é: " + idade);
     }
 }
