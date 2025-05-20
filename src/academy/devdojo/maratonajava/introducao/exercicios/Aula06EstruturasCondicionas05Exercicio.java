b62bf6eed66be0e27cfb7c0a3227565ce03f6e73
Arthur Pessoa <arthhpb@gmail.com>
2025-05-20 20:01:39 -0300

    Adicionada a aula de Switch-Case em conjunto com o exercício da aula.

---
+++ src/academy/devdojo/maratonajava/introducao/exercicios/Aula06EstruturasCondicionas05Exercicio.java
@@ -0,0 +1,47 @@
+package academy.devdojo.maratonajava.introducao.exercicios;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Aula06EstruturasCondicionas05Exercicio {
+    public static void main(String[] args) {
+        // Utilizando Switch-Case
+        // Dados os valores de 1 a 7, imprima se é dia útil ou final de semana
+        // considerando 1 como domingo.
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Os dias vão de 1 a 7, considerando 1 como domingo!");
+        System.out.print("Insira o um número: ");
+        byte dia = input.nextByte();
+        switch (dia)
+        {
+            case 1:
+                System.out.println("Domingo é final de semana.");
+                break;
+            case 2:
+                System.out.println("Segunda é dia útil.");
+                break;
+            case 3:
+                System.out.println("Terça é dia útil.");
+                break;
+            case 4:
+                System.out.println("Quarta é dia útil.");
+                break;
+            case 5:
+                System.out.println("Quinta é dia útil.");
+                break;
+            case 6:
+                System.out.println("Sexta é dia útil.");
+                break;
+            case 7:
+                System.out.println("Sábado é final de semana.");
+                break;
+            default:
+                System.out.println("Dia inválido.");
+                break;
+        }
+
+    }
+}
