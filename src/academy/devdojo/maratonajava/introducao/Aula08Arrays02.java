d7ea18e92935b66b2da1f94d2273fcb2eb5ed4bb
Arthur Pessoa <arthhpb@gmail.com>
2025-05-23 20:24:51 -0300

    Adicionadas as aulas 1 e 2 de arrays

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08Arrays02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double -> 0
+        // char -> u000 (retorna um espaço em branco)
+        // boolean -> false
+        // String -> null
+        int[] idades = new int[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
