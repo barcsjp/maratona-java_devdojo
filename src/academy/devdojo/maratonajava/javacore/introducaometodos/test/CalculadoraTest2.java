023f0b8b80732a757cc0f1768fdc8d49d0f15608
barcsjp <arthur.pessoab@outlook.com>
2025-06-04 19:35:13 -0300

    Adicionado o conteúdo da aula 2 de métodos

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest2.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest2.java
@@ -2,12 +2,9 @@ package academy.devdojo.maratonajava.javacore.introducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.introducaometodos.domain.Calculadora;
 
-public class CalculadoraTest {
+public class CalculadoraTest2 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.soma();
-        System.out.println("Finalizando CalculadoraTest");
-        calculadora.subtraiDoisNumeros();
-
+        calculadora.multiplicaDoisNumeros(2, 3);
     }
 }
